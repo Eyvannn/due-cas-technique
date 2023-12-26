fd2d2b01b352d80a192fa53dede3bc1b74ea5f23
Eyvan <109107748+Eyvannn@users.noreply.github.com>
2023-12-26 17:09:54 +0100

    feat: add parsing attributes items in the db

--- main.rb
+++ main.rb
@@ -45,15 +45,32 @@ workbook.worksheets.each do |worksheet|
         item_id = item_id.to_i + 1
       end
 
-      connexion_bd.exec_params('INSERT INTO items (itemid, packageid) VALUES ($1, $2)', [item_id, package_id])
+      #connexion_bd.exec_params('INSERT INTO items (itemid, packageid) VALUES ($1, $2)', [item_id, package_id])
+      connexion_bd.exec_params('INSERT INTO items (itemid, packageid, warranty) VALUES ($1, $2, false)', [item_id, package_id])
 
       current_item = row[1].value
+    end
+
+    case row[2].value
+    when "name"
+      connexion_bd.exec_params('UPDATE items SET name=$1 WHERE itemid=$2;', [row[3].value, item_id])
+    when "price"
+      connexion_bd.exec_params('UPDATE items SET price=$1 WHERE itemid=$2;', [row[3].value, item_id])
+    when "ref"
+      connexion_bd.exec_params('UPDATE items SET ref=$1 WHERE itemid=$2;', [row[3].value, item_id])
+    when "warranty"
+      if row[3].value.downcase == "yes"
+        connexion_bd.exec_params('UPDATE items SET warranty=true WHERE itemid=$1;', [item_id])
+      elsif row[3].value.downcase == "no" || row[3].value.length == 0
+        connexion_bd.exec_params('UPDATE items SET warranty=false WHERE itemid=$1;', [item_id])
+      end
+    when "duration"
+      connexion_bd.exec_params('UPDATE items SET duration=$1 WHERE itemid=$2;', [row[3].value, item_id])
     end
 
     #connexion_bd.exec_params('INSERT INTO items (orderid, ordername) VALUES ($1, $2)', [col1, col2])
   end
 end
-
 
 
 #resultat = OutilsPostgreSQL.exec_simple_requete(connexion_bd, "SELECT * FROM orders;")
